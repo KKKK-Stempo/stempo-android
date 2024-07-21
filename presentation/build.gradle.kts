7be5add9584ebb48665e2ad56fa2aeae5e5cbbf9
Sangho Kim <marchbreeze03@gmail.com>
2024-07-21 17:58:26 +0900

    [ADD/#1] presentation 모듈 초기 세팅

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "kr.genti.presentation"
+    compileSdk = Constants.compileSdk
+
+    defaultConfig {
+        minSdk = Constants.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = Versions.javaVersion
+        targetCompatibility = Versions.javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmVersion
+    }
+
+    buildFeatures {
+        buildConfig = true
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":domain"))
+
+    KotlinDependencies.run {
+        implementation(kotlin)
+        implementation(coroutines)
+        implementation(jsonSerialization)
+        implementation(dateTime)
+    }
+
+    AndroidXDependencies.run {
+        implementation(coreKtx)
+        implementation(appCompat)
+        implementation(constraintLayout)
+        implementation(fragment)
+        implementation(navigationFragment)
+        implementation(navigationUi)
+        implementation(startup)
+        implementation(legacy)
+        implementation(security)
+        implementation(hilt)
+        implementation(lifeCycleKtx)
+        implementation(lifecycleJava8)
+        implementation(splashScreen)
+        implementation(workManager)
+        implementation(hiltWorkManager)
+    }
+
+    KaptDependencies.run {
+        kapt(hiltCompiler)
+        kapt(hiltWorkManagerCompiler)
+    }
+
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    TestDependencies.run {
+        testImplementation(jUnit)
+        androidTestImplementation(androidTest)
+        androidTestImplementation(espresso)
+    }
+
+    ThirdPartyDependencies.run {
+        implementation(coil)
+        implementation(timber)
+        implementation(ossLicense)
+        implementation(progressView)
+        implementation(balloon)
+        implementation(lottie)
+        implementation(circularProgressBar)
+        implementation(circleIndicator)
+    }
+}
