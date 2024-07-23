02a9c299385c56bf7af3a497c812847a5bd701aa
Sangho Kim <marchbreeze03@gmail.com>
2024-07-23 17:51:22 +0900

    [FEAT/#5] 메인뷰 바텀네비게이션 설정

--- presentation/src/main/java/com/kkkk/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/MainActivity.kt
@@ -1,2 +1,59 @@
 package com.kkkk.presentation.main
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.kkkk.core.base.BaseActivity
+import com.kkkk.presentation.main.profile.ProfileFragment
+import com.kkkk.presentation.main.report.ReportFragment
+import com.kkkk.presentation.main.rhythm.RhythmFragment
+import com.kkkk.presentation.main.study.StudyFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBnvItemIconTintList()
+        initBnvItemSelectedListener()
+    }
+
+    fun initBnvItemIconTintList() {
+        with(binding.bnvMain) {
+            itemIconTintList = null
+            selectedItemId = R.id.menu_rhythm
+        }
+    }
+
+    private fun initBnvItemSelectedListener() {
+        supportFragmentManager.findFragmentById(R.id.fcv_main) ?: navigateTo<RhythmFragment>()
+
+        binding.bnvMain.setOnItemSelectedListener { menu ->
+            if (binding.bnvMain.selectedItemId == menu.itemId) {
+                return@setOnItemSelectedListener false
+            }
+            when (menu.itemId) {
+                R.id.menu_rhythm -> navigateTo<RhythmFragment>()
+
+                R.id.menu_report -> navigateTo<ReportFragment>()
+
+                R.id.menu_study -> navigateTo<StudyFragment>()
+
+                R.id.menu_profile -> navigateTo<ProfileFragment>()
+
+                else -> return@setOnItemSelectedListener false
+            }
+            true
+        }
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.fcv_main, T::class.java.canonicalName)
+        }
+    }
+}
