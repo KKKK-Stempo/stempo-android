1e19eea81bbe5469dc20dd4f313e13065e731475
Dongmin <pdm001125@naver.com>
2024-07-24 04:51:14 +0900

    [FEAT/#12] set OnboardingMeasureFragment

--- presentation/src/main/java/com/kkkk/presentation/main/onboarding/onbarding/OnboardingMeasureFragment.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/onboarding/onbarding/OnboardingMeasureFragment.kt
@@ -1,11 +1,8 @@
 package com.kkkk.presentation.main.onboarding.onbarding
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.kkkk.core.base.BaseFragment
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.FragmentOnboardingMeasureBinding
@@ -14,16 +11,17 @@ class OnboardingMeasureFragment :
     BaseFragment<FragmentOnboardingMeasureBinding>(R.layout.fragment_onboarding_measure) {
     private val viewModel by activityViewModels<OnboardingViewModel>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_onboarding_measure, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initButtonListener()
+    }
+
+    private fun initButtonListener() {
+        with(binding) {
+            btnOnboardingMeasureLater.setOnClickListener {
+                viewModel.setState(OnboardingState.DONE)
+            }
+        }
     }
 }
