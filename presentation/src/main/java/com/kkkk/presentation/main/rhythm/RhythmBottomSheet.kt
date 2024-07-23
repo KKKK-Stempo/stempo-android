53f51d1dde23c17cd9ec8b47ae9d6aacb1905f35
Sangho Kim <marchbreeze03@gmail.com>
2024-07-23 22:49:26 +0900

    [ADD/#7] 리듬레벨 선택 바텀시트 설정

---
+++ presentation/src/main/java/com/kkkk/presentation/main/rhythm/RhythmBottomSheet.kt
@@ -0,0 +1,24 @@
+package com.kkkk.presentation.main.rhythm
+
+import android.os.Bundle
+import android.view.View
+import com.kkkk.core.base.BaseBottomSheet
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.BottomSheetRhythmBinding
+
+class MobileBottomSheet :
+    BaseBottomSheet<BottomSheetRhythmBinding>(R.layout.bottom_sheet_rhythm) {
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setBackgroundDrawableResource(R.color.transparent)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
