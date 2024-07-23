2b0e31e7d308e354d1cb6e67420e476c9e0f1380
Sangho Kim <marchbreeze03@gmail.com>
2024-07-24 02:23:39 +0900

    [ADD/#9] 기록뷰 그래프 초기 설정

--- presentation/src/main/java/com/kkkk/presentation/main/report/ReportFragment.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/report/ReportFragment.kt
@@ -2,7 +2,14 @@ package com.kkkk.presentation.main.report
 
 import android.os.Bundle
 import android.view.View
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.kkkk.core.base.BaseFragment
+import com.kkkk.core.extension.colorOf
+import com.kkkk.core.extension.dpToPx
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.FragmentReportBinding
@@ -15,5 +22,47 @@ class ReportFragment : BaseFragment<FragmentReportBinding>(R.layout.fragment_rep
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        setGraphData()
+        setGraphSettings()
+    }
+
+    private fun setGraphData() {
+        val chartEntry = arrayListOf<Entry>()
+        val mockList = listOf(20, 30, 50, 90)
+        mockList.forEachIndexed { index, listItem ->
+            chartEntry.add(Entry(index.toFloat(), listItem.toFloat()))
+        }
+
+        binding.chartReport.apply {
+            xAxis.valueFormatter = IndexAxisValueFormatter(listOf("1월", "2월", "3월", "4월"))
+            data = LineData(LineDataSet(chartEntry, "").apply {
+                color = colorOf(R.color.purple_50)
+                circleRadius = 8f
+                setCircleColor(colorOf(R.color.purple_50))
+                setDrawFilled(true)
+                setDrawValues(false)
+                lineWidth = 3F
+                setDrawCircleHole(false)
+                fillColor = colorOf(R.color.purple_10)
+                mode = LineDataSet.Mode.LINEAR
+            })
+            invalidate()
+        }
+    }
+
+    private fun setGraphSettings() {
+        binding.chartReport.apply {
+            xAxis.apply {
+                position = XAxis.XAxisPosition.BOTTOM
+                setDrawGridLines(false)
+                setDrawAxisLine(false)
+                textColor = colorOf(R.color.dark)
+                setAvoidFirstLastClipping(true)
+            }
+            axisLeft.isEnabled = false
+            axisRight.isEnabled = false
+            legend.isEnabled = false
+            description.isEnabled = false
+        }
     }
 }
