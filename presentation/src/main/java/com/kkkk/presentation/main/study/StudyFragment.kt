c8eb6fd4ffba738b7674b4feabc55e4cee9f0685
Dongmin <pdm001125@naver.com>
2024-07-24 14:37:00 +0900

    [FEAT/#14] init TabLayout and ViewPager2

--- presentation/src/main/java/com/kkkk/presentation/main/study/StudyFragment.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/study/StudyFragment.kt
@@ -9,11 +9,24 @@ import kr.genti.presentation.databinding.FragmentStudyBinding
 
 @AndroidEntryPoint
 class StudyFragment : BaseFragment<FragmentStudyBinding>(R.layout.fragment_study) {
+    private val tabTextList = listOf("재활운동", "정보")
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        setTabLayout()
+    }
+
+    private fun setTabLayout() {
+        binding.tabStudy.apply {
+            for (tabName in tabTextList) {
+                val tab = this.newTab()
+                tab.text = tabName
+                this.addTab(tab)
+            }
+        }
     }
 }
