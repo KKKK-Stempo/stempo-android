d6a5d165a171598955b0b8987c043646989ce5d8
Sangho Kim <marchbreeze03@gmail.com>
2024-07-31 02:47:03 +0900

    [FEAT/#28] SharedPreference의 level 설정

--- presentation/src/main/java/com/kkkk/presentation/main/rhythm/RhythmViewModel.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/rhythm/RhythmViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kkkk.core.state.UiState
 import com.kkkk.domain.repository.RhythmRepository
+import com.kkkk.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,7 +16,8 @@ import javax.inject.Inject
 class RhythmViewModel
 @Inject
 constructor(
-    private val rhythmRepository: RhythmRepository
+    private val rhythmRepository: RhythmRepository,
+    private val userRepository: UserRepository
 ) : ViewModel() {
     var tempRhythmLevel = MutableLiveData<Int>(1)
     var filename: String = "stempo_level_1"
@@ -29,6 +31,15 @@ constructor(
     private val _downloadWavState = MutableStateFlow<UiState<ByteArray>>(UiState.Empty)
     val downloadWavState: StateFlow<UiState<ByteArray>> = _downloadWavState
 
+    init {
+        initRhythmLevelFromDataStore()
+    }
+
+    private fun initRhythmLevelFromDataStore() {
+        filename = "stempo_level_" + userRepository.getBpmLevel().toString()
+        _rhythmLevel.value = userRepository.getBpmLevel()
+    }
+
     fun setTempRhythmLevel(level: Int) {
         tempRhythmLevel.value = level
     }
@@ -36,6 +47,7 @@ constructor(
     fun setRhythmLevel() {
         filename = "stempo_level_" + tempRhythmLevel.value.toString()
         _rhythmLevel.value = tempRhythmLevel.value ?: -1
+        userRepository.setBpmLevel(rhythmLevel.value)
     }
 
     fun postToGetRhythmUrlFromServer(level: Int) {
