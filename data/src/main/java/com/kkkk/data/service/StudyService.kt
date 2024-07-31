2b9872d93854975ec10abef1e3b404d99591725b
Dongmin <pdm001125@naver.com>
2024-07-31 10:56:51 +0900

    [FEATURE/#27] GET videos service

--- data/src/main/java/com/kkkk/data/service/StudyService.kt
+++ data/src/main/java/com/kkkk/data/service/StudyService.kt
@@ -2,9 +2,20 @@ package com.kkkk.data.service
 
 import com.kkkk.data.dto.BaseResponse
 import com.kkkk.data.dto.response.VideoDto
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface StudyService {
-    @POST("api/v1/videos")
-    suspend fun getVideos(): BaseResponse<VideoDto>
+    @GET("api/v1/videos")
+    suspend fun getVideos(
+        @Query("page")
+        page: Int,
+        @Query("size")
+        size: Int,
+        @Query("sortBy")
+        sortBy: String,
+        @Query("sortDirection")
+        sortDirection: String,
+    ): BaseResponse<VideoDto>
 }
