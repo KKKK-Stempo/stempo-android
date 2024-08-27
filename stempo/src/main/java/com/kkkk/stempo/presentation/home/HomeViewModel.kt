2ad70c7635767866bf5d8798676822db4d09fd78
Dongmin <pdm001125@naver.com>
2024-08-27 22:18:17 +0900

    [FEAT/#42] feat Screen & ViewModel

---
+++ stempo/src/main/java/com/kkkk/stempo/presentation/home/HomeViewModel.kt
@@ -0,0 +1,57 @@
+package com.kkkk.stempo.presentation.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+    private val _state: MutableStateFlow<HomeState> = MutableStateFlow(HomeState())
+    val state: StateFlow<HomeState>
+        get() = _state.asStateFlow()
+
+    private val _sideEffect: MutableSharedFlow<HomeSideEffect> = MutableSharedFlow()
+    val sideEffect: SharedFlow<HomeSideEffect>
+        get() = _sideEffect.asSharedFlow()
+
+
+    private var vibrationJob: Job? = null
+
+    fun controlMusic() {
+        _state.value = _state.value.copy(isPlayingMusic = !_state.value.isPlayingMusic)
+
+        if (_state.value.isPlayingMusic) {
+            startVibration()
+        } else {
+            stopVibration()
+        }
+    }
+
+    private fun startVibration() {
+        if (vibrationJob?.isActive == true) return
+
+        vibrationJob = viewModelScope.launch {
+            while (true) {
+                _sideEffect.emit(HomeSideEffect.Vibrate)
+                delay(VIBRATION_INTERVAL)
+            }
+        }
+    }
+
+    private fun stopVibration() {
+        vibrationJob?.cancel()
+        vibrationJob = null
+    }
+
+    companion object {
+        const val VIBRATION_INTERVAL = 500L // TODO: Server에서 받아올 예정
+        const val VIBRATION_DURATION = 100L
+    }
+}
