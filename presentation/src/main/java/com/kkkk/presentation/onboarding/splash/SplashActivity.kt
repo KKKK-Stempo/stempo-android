77db613b6b30e98a9a8ef1806128d15c17deeb96
Dongmin <pdm001125@naver.com>
2024-09-22 02:46:51 +0900

    [FEAT/#55] check permission dialog and setting

--- presentation/src/main/java/com/kkkk/presentation/onboarding/splash/SplashActivity.kt
+++ presentation/src/main/java/com/kkkk/presentation/onboarding/splash/SplashActivity.kt
@@ -1,9 +1,21 @@
 package com.kkkk.presentation.onboarding.splash
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.kkkk.core.base.BaseActivity
@@ -29,8 +41,15 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         setStatusBarColor()
         setNavigationBarColor()
         observeStates()
+    }
 
-        viewModel.checkTokenState()
+    override fun onResume() {
+        super.onResume()
+        if (isActivityRecognitionPermissionGranted(this)) {
+            viewModel.checkTokenState()
+        } else {
+            showDialog()
+        }
     }
 
     private fun setStatusBarColor() = setStatusBarColorFromResource(R.color.purple_50)
@@ -63,6 +82,65 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
 
     private fun login() {
         viewModel.setAndroidId(getDeviceTag())
+    }
+
+    private fun isActivityRecognitionPermissionGranted(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACTIVITY_RECOGNITION
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            // Android Q 미만 버전에서는 이 권한이 필요하지 않으므로 항상 true 반환
+            true
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_single_button)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.setCancelable(false)
+        dialog.show()
+
+        dialog.findViewById<View>(R.id.btn_onboarding_start_measure).setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                    200
+                )
+            } else {
+                navigateToSettings()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 200) {
+            if (permissions.isNotEmpty() && permissions[0] == Manifest.permission.ACTIVITY_RECOGNITION) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    viewModel.checkTokenState()
+                } else {
+                    navigateToSettings()
+                }
+            }
+        }
+    }
+
+    private fun navigateToSettings() {
+        Intent().apply {
+            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            data = Uri.fromParts("package", packageName, null)
+            startActivity(this)
+        }
     }
 
     @SuppressLint("HardwareIds")
