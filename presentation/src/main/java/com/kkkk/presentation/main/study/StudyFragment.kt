111415e049e1b292c6395fa6a625cf2b4303719f
Dongmin <pdm001125@naver.com>
2024-09-22 15:26:18 +0900

    [FEAT/#57] feat custom toggle clickListener

--- presentation/src/main/java/com/kkkk/presentation/main/study/StudyFragment.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/study/StudyFragment.kt
@@ -1,10 +1,11 @@
 package com.kkkk.presentation.main.study
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import com.kkkk.core.base.BaseFragment
-import com.kkkk.core.extension.colorOf
 import com.kkkk.core.extension.setStatusBarColor
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.presentation.R
@@ -20,26 +21,27 @@ class StudyFragment : BaseFragment<FragmentStudyBinding>(R.layout.fragment_study
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-        setTabLayout()
-        setViewPager()
-        setStatusBarColor(R.color.white)
+        setStatusBarColor(R.color.gray_100)
+        setToggleClickListener()
     }
 
-    private fun setTabLayout() {
-        binding.tabStudy.apply {
-            for (tabName in tabTextList) {
-                val tab = this.newTab()
-                tab.text = tabName
-                this.addTab(tab)
+    @SuppressLint("ResourceAsColor")
+    private fun setToggleClickListener() {
+        binding.itemToggle.tvTeacher.setOnClickListener {
+            with(binding.itemToggle) {
+                tvTeacher.setBackgroundResource(R.drawable.shape_toggle_selected)
+                tvTeacher.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                tvStudent.background = null
+                tvStudent.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
             }
         }
-    }
-
-    private fun setViewPager() {
-        binding.vpStudy.adapter = StudyViewPagerAdapter(requireActivity())
-        binding.vpStudy.isUserInputEnabled = false
-        TabLayoutMediator(binding.tabStudy, binding.vpStudy) { tab, pos ->
-            tab.text = tabTextList[pos]
-        }.attach()
+        binding.itemToggle.tvStudent.setOnClickListener {
+            with(binding.itemToggle) {
+                tvStudent.setBackgroundResource(R.drawable.shape_toggle_selected)
+                tvStudent.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                tvTeacher.background = null
+                tvTeacher.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            }
+        }
     }
 }
