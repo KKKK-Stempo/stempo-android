79cf8085ab4be8e062e592584f7df302d1a50160
Dongmin <pdm001125@naver.com>
2025-01-03 01:16:32 +0900

    feature #82: feat terms activity

---
+++ presentation/src/main/java/com/kkkk/presentation/onboarding/terms/TermsActivity.kt
@@ -0,0 +1,43 @@
+package com.kkkk.presentation.onboarding.terms
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import com.kkkk.core.base.BaseActivity
+import com.kkkk.core.extension.navigateToScreenClear
+import com.kkkk.presentation.onboarding.onbarding.OnboardingActivity
+import com.kkkk.stempo.presentation.R
+import com.kkkk.stempo.presentation.databinding.ActivityTermsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TermsActivity : BaseActivity<ActivityTermsBinding>(R.layout.activity_terms) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.tvTerms.setOnClickListener {
+            navigateToWeb(URL_TERMS)
+        }
+
+        binding.tvPersonalInformation.setOnClickListener {
+            navigateToWeb(URL_PERSONAL_INFORMATION)
+        }
+
+        binding.btnTermsAgree.setOnClickListener {
+            navigateToScreenClear<OnboardingActivity>()
+        }
+    }
+
+    private fun navigateToWeb(url: String) {
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+            startActivity(this)
+        }
+    }
+
+    companion object {
+        private const val URL_TERMS =
+            "https://field-colt-189.notion.site/Stempo-1658e42622af4c7ab343db3f81d6006b?pvs=4"
+        private const val URL_PERSONAL_INFORMATION =
+            "https://field-colt-189.notion.site/Stempo-a3dfaebc01a744d19a6ddcd4a75917f6?pvs=4"
+    }
+}
