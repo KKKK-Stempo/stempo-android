360cfe046e5a99e7a61699d0b1e1bbd25cd19e8d
Dongmin <pdm001125@naver.com>
2025-01-05 02:22:41 +0900

    [FEAT/#87] add colors

--- presentation/src/main/java/com/kkkk/presentation/main/theme/Color.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/theme/Color.kt
@@ -9,3 +9,38 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Monochrome
+val White = Color(0xFFFFFFFF)
+val Gray100 = Color(0xFFF7F7F7)
+val Gray200 = Color(0xFFEDEDED)
+val Gray300 = Color(0xFFDEDEDE)
+val Gray400 = Color(0xFFCCCCCC)
+val Gray500 = Color(0xFFAEAEAE)
+val Gray600 = Color(0xFF8D8D8D)
+val Gray700 = Color(0xFF6B6B6B)
+val Gray800 = Color(0xFF4A4A4A)
+val Gray900 = Color(0xFF2D2D2D)
+val Black = Color(0xFF0E0E0E)
+
+// Primary
+val Purple10 = Color(0xFFE0DAF8)
+val Purple50 = Color(0xFF7052E9)
+val Purple100 = Color(0xFF4930AC)
+
+// Secondary
+val Sky10 = Color(0xFFDAF3FB)
+val Sky50 = Color(0xFF54D1F9)
+val Green10 = Color(0xFFE5F6CC)
+val Green50 = Color(0xFF8BDC0A)
+
+// System
+val Info = Color(0xFF368FF7)
+val Dark = Color(0xFF1E3037)
+
+// Transparent
+val Transparent = Color(0x00000000)
+val Transparent70 = Color(0xB3000000)
+val Transparent60 = Color(0x99000000)
+val Transparent50 = Color(0x80000000)
+val Transparent30 = Color(0x4D000000)
