86647a259f17881c3e8b0f1cfcfc6ba7ba9be06c
Sangho Kim <marchbreeze03@gmail.com>
2025-01-06 22:23:22 +0900

    [ADD/#91] 리듬변경 바텀시트 Bpm 구현

--- presentation/src/main/java/com/kkkk/presentation/main/rhythm/component/RhythmBottomSheet.kt
+++ presentation/src/main/java/com/kkkk/presentation/main/rhythm/component/RhythmBottomSheet.kt
@@ -1,22 +1,24 @@
 package com.kkkk.presentation.main.rhythm.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -29,15 +31,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kkkk.presentation.main.rhythm.RhythmState
+import com.kkkk.presentation.main.rhythm.RhythmState.Companion.MAX_BPM
+import com.kkkk.presentation.main.rhythm.RhythmState.Companion.MIN_BPM
+import com.kkkk.presentation.main.theme.Gray100
 import com.kkkk.presentation.main.theme.Gray200
 import com.kkkk.presentation.main.theme.Gray300
 import com.kkkk.presentation.main.theme.Gray500
 import com.kkkk.presentation.main.theme.Gray600
+import com.kkkk.presentation.main.theme.Purple10
 import com.kkkk.presentation.main.theme.Purple50
 import com.kkkk.presentation.main.theme.StempoTheme
 import com.kkkk.presentation.main.theme.White
@@ -88,7 +95,7 @@ fun RhythmBottomSheet(
                 style = StempoTheme.typography.head4,
                 modifier = Modifier
                     .padding(top = 10.dp)
-                    .padding(horizontal = 20.dp)
+                    .padding(horizontal = 30.dp)
             )
 
             LazyVerticalGrid(
@@ -117,10 +124,88 @@ fun RhythmBottomSheet(
             Text(
                 text = "빠르기 선택",
                 style = StempoTheme.typography.head4,
-                modifier = Modifier.padding(horizontal = 20.dp)
+                modifier = Modifier.padding(horizontal = 30.dp)
             )
 
-            
+            Row(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                IconButton(
+                    onClick = { if (tempBpm > MIN_BPM) tempBpm -= 5 },
+                    enabled = tempBpm > MIN_BPM
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(
+                            id = if (tempBpm > MIN_BPM) {
+                                R.drawable.ic_minus_purple
+                            } else {
+                                R.drawable.ic_minus_gray
+                            }
+                        ),
+                        contentDescription = null
+                    )
+                }
+                Text(
+                    text = "$tempBpm",
+                    style = StempoTheme.typography.head1,
+                )
+                IconButton(
+                    onClick = { if (tempBpm < MAX_BPM) tempBpm += 5 },
+                    enabled = tempBpm < MAX_BPM
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(
+                            id = if (tempBpm < MAX_BPM) {
+                                R.drawable.ic_plus_purple
+                            } else {
+                                R.drawable.ic_plus_gray
+                            }
+                        ),
+                        contentDescription = null
+                    )
+                }
+            }
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                modifier = Modifier
+                    .padding(top = 24.dp)
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                items(listOf(65, 75, 85, 95, 105, 115)) { bpm ->
+                    RhythmBpmItem(
+                        bpm = bpm,
+                        isSelected = tempBpm == bpm,
+                        onBpmSelected = {tempBpm = bpm}
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .padding(bottom = 16.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .background(
+                        shape = RoundedCornerShape(12.dp),
+                        color = Purple10
+                    )
+                    .clickableWithoutRipple { onSubmitBtnClick(tempBit, tempBpm) }
+                    .padding(vertical = 15.dp),
+                text = stringResource(R.string.rhythm_btn_submit_level),
+                textAlign = TextAlign.Center,
+                color = Purple50,
+                style = StempoTheme.typography.head4
+            )
         }
     }
 }
@@ -144,6 +229,34 @@ fun RhythmBitItem(
             text = "$bit 박자",
             style = StempoTheme.typography.head2,
             color = if (isSelected) White else Gray600
+        )
+    }
+}
+
+@Composable
+fun RhythmBpmItem(
+    bpm: Int,
+    isSelected: Boolean,
+    onBpmSelected: (Int) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(25.dp))
+            .background(if (isSelected) White else Gray100)
+            .border(
+                width = 2.dp,
+                color = if (isSelected) Purple50 else Gray300,
+                shape = RoundedCornerShape(25.dp)
+            )
+            .clickableWithoutRipple { onBpmSelected(bpm) }
+            .padding(vertical = 12.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = bpm.toString(),
+            color = if (isSelected) Purple50 else Gray500,
+            style = StempoTheme.typography.head3
         )
     }
 }
