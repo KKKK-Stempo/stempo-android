68f71b56b1d29663a4e7635c279f147b086f6a63
Sangho Kim <marchbreeze03@gmail.com>
2025-02-16 01:05:17 +0900

    [ADD/#108] AmplitudeManager 세팅

---
+++ presentation/src/main/java/com/kkkk/presentation/manager/AmplitudeManager.kt
@@ -0,0 +1,64 @@
+package com.kkkk.presentation.manager
+
+import android.content.Context
+import com.amplitude.android.Amplitude
+import com.amplitude.android.Configuration
+import com.amplitude.android.events.Identify
+
+object AmplitudeManager {
+    private lateinit var amplitude: Amplitude
+
+    fun init(
+        context: Context,
+        apiKey: String,
+    ) {
+        amplitude =
+            Amplitude(
+                Configuration(
+                    apiKey = apiKey,
+                    context = context.applicationContext,
+                ),
+            )
+    }
+
+    fun trackEvent(
+        eventName: String,
+        properties1: Map<String, Any>? = null,
+        properties2: Map<String, Any>? = null,
+    ) {
+        when {
+            properties1 == null && properties2 == null -> {
+                amplitude.track(eventName)
+            }
+
+            properties1 != null && properties2 == null -> {
+                amplitude.track(eventName, properties1)
+            }
+
+            properties1 != null && properties2 != null -> {
+                val combinedProperties = mutableMapOf<String, Any?>()
+                combinedProperties.putAll(properties1)
+                combinedProperties.putAll(properties2)
+                amplitude.track(eventName, combinedProperties)
+            }
+        }
+    }
+
+    fun updateProperties(
+        propertyName: String,
+        values: String,
+    ) {
+        amplitude.identify(Identify().set(propertyName, values))
+    }
+
+    fun updateIntProperties(
+        propertyName: String,
+        intValues: Int,
+    ) {
+        amplitude.identify(Identify().set(propertyName, intValues))
+    }
+
+    fun plusIntProperties(propertyName: String) {
+        amplitude.identify(Identify().add(propertyName, 1))
+    }
+}
